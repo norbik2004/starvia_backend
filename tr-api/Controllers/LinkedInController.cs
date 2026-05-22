5f59e3b9e716431949126c10675e54c251096c92
Norbert <kieler103@gmail.com>
2026-05-22 17:19:44 +0200

    fix: #dev fix redirect

--- tr-api/Controllers/LinkedInController.cs
+++ tr-api/Controllers/LinkedInController.cs
@@ -63,7 +63,7 @@ namespace tr_backend.Controllers
             // Someday redirecting to frontend with UserPlatform object
             _ = await userPlatformService.AddUserPlatformAsync(request, userId);
 
-            return Redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1");
+            return Redirect(appsettingsConfig["LinkedIn:SuccessRedirectUrl"]!);
         }
 
     }
