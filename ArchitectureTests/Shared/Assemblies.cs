d1645f1adf881f383c9197c84619f74c049db937
Norbert <kieler103@gmail.com>
2026-06-14 22:55:04 +0200

    feat: #17-architecture-tests Project structure

---
+++ ArchitectureTests/Shared/Assemblies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ArchitectureTests.Shared
+{
+    public static class Assemblies
+    {
+        public static readonly Assembly CoreApplication =
+        typeof(Core.Application.AssemblyReference).Assembly;
+    }
+}
